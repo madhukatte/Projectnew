b9b94350969bd377906371185ad4d17bf5b287d9
mkhr <mkhr@MKHR-IN.in.oracle.com>
2020-08-12 19:08:41 +0530

    changes

--- MaxToy.java
+++ MaxToy.java
@@ -1,34 +1,37 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class MaxToy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int payAmt = 25; 
-		int cost[] = {1, 10, 5, 3, 0}; 
-		int costLen = cost.length; 
-		  
-		System.out.print( maxToys(cost, costLen, payAmt));
+		Scanner stdin = new Scanner(System.in);
+		int payAmt = stdin.nextInt();
+		int cost[] = new int[5];
+		//Assume 5 toys are available  and cost is initiatilized
+		for (int i = 0; i < 5; i++) {
+			cost[i] = stdin.nextInt();
+		}		
+		int costLen = cost.length;
+		System.out.print("maxtoys--"+maxToys(cost, costLen, payAmt));
 	}
 
 	
 	
+	
 	private static int maxToys(int[] cost, int costLen, int payAmt) {
-		int count = 0, sum = 0; 	  
-	     
-	    Arrays.sort(cost); 
-	    for (int i = 0; i < costLen; i++)     { 
-	  
-	          
-	        if (sum +cost[i] <= payAmt)  
-	        { 
-	            sum = sum + cost[i];
-	             
-	            count++; 
-	        } 
-	    } 
-	    return count; 		
-		
+		int count = 0, sum = 0;
+		Arrays.sort(cost);
+		for (int i = 0; i < costLen; i++) {
+			if (sum + cost[i]  <= payAmt) {
+				System.out.println("testing"+cost[i]);
+				sum = sum + cost[i];
+				count++;
+			}
+		}
+		return count;
+
 	}
 
 }
