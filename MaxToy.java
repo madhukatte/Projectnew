de6e48c74622d626547be0e84d45d4eb5c9097d5
mkhr <mkhr@MKHR-IN.in.oracle.com>
2020-08-14 10:19:51 +0530

    changes added comments

--- MaxToy.java
+++ MaxToy.java
@@ -11,23 +11,36 @@ import java.util.stream.Collectors;
 
 public class MaxToy {
 
+	/**Main method max toys program
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner stdin = new Scanner(System.in);
-
+        // max amount for purchase
 		int payAmt = stdin.nextInt();
+		//cost of toys
 		int toycost[] = { 5, 1, 3, 2, 8 };
 		int costLen = toycost.length;
 		// Available toys
 		int availableToys[] = new int[toycost.length];
+		
+		//quantity of each toys initiatized
 		for (int i = 0; i < availableToys.length; i++) {
 			availableToys[i] = stdin.nextInt();
 		}
 
-		// Map<Integer, Integer> toysAvailableMap =
+		
 		System.out.print("maxtoys purchased--" + maxToys(toycost, costLen, payAmt, buildMap(availableToys)));
 	}
 
-	private static Map buildMap(int[] toyCost) {
+	
+	/**
+	 * Method to create Map for available toys
+	 * @param toyCost
+	 * @return
+	 */
+	private static Map<Integer, Integer> buildMap(int[] toyCost) {
 
 		Map<Integer, Integer> createMap = new HashMap<>();
 
@@ -36,6 +49,8 @@ public class MaxToy {
 			createMap.put(Integer.valueOf(i), Integer.valueOf(toyCost[i]));
 
 		}
+		
+		//sorted the Toys quantity 
 		Map<Integer, Integer> sortedMap = (Map<Integer, Integer>) createMap.entrySet().stream()
 				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
 				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
@@ -43,6 +58,15 @@ public class MaxToy {
 		return sortedMap;
 	}
 
+	
+	/**
+	 * logic to sort cost of toys  and calculate max purchased toys for specified amount
+	 * @param cost
+	 * @param costLen
+	 * @param payAmt
+	 * @param toysAvailableMap
+	 * @return
+	 */
 	private static int maxToys(int[] cost, int costLen, int payAmt, Map<Integer, Integer> toysAvailableMap) {
 		int count = 0, sum = 0, tempAmount = 0, quantity = 0, purchasedQuantity = 0, newQuantity;
 		Arrays.sort(cost);
